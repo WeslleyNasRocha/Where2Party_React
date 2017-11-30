c4eaedc1ebe7f7e4d250733799098eaea2a369f6
Weslley Rocha <weslleytato.12@gmail.com>
2017-11-30 12:18:58 -0200

    Merge remote-tracking branch 'origin/develop' into develop

--- src/Components/Event/Components/SearchMap.js
+++ src/Components/Event/Components/SearchMap.js
@@ -24,12 +24,7 @@ class SearchMap extends Component {
   }
 
   setLocale = () => {
-    const {
-      latitude,
-      longitude,
-      latitudeDelta,
-      longitudeDelta
-    } = this.state.location.latlng;
+    const { latitude, longitude, latitudeDelta, longitudeDelta } = this.state.location.latlng;
     this.props.saveGpsLocation({
       latitude,
       longitude,
@@ -39,23 +34,21 @@ class SearchMap extends Component {
     });
   };
 
-  renderMap = location => {
+  renderMap = location => (
     // console.log('changed');
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        region={location.latlng}
-        ref={ref => (this.mapa = ref)}
-        rotateEnabled={false}
-        scrollEnabled={false}
-        scrollEnabled={false}
-        loadingEnabled
-        toolbarEnabled
-      >
-        <MapView.Marker coordinate={location.latlng} title={location.title} />
-      </MapView>
-    );
-  };
+    <MapView
+      style={{ flex: 1 }}
+      region={location.latlng}
+      ref={ref => (this.mapa = ref)}
+      rotateEnabled={false}
+      scrollEnabled={false}
+      scrollEnabled={false}
+      loadingEnabled
+      toolbarEnabled
+    >
+      <MapView.Marker coordinate={location.latlng} title={location.title} />
+    </MapView>
+  );
 
   render() {
     const apiGooglePlaces = 'AIzaSyBHA0EjLHKYsO4J-vm34zWlrpL40sMUf9I';
@@ -71,7 +64,7 @@ class SearchMap extends Component {
         >
           <GooglePlacesAutocomplete
             placeholder="Search"
-            minLength={2} // minimum length of text to search
+            minLength={3} // minimum length of text to search
             autoFocus={false}
             returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
             listViewDisplayed="auto" // true/false/undefined
@@ -116,31 +109,32 @@ class SearchMap extends Component {
                 marginTop: 0
               },
               description: {
-                fontWeight: 'bold'
+                fontWeight: 'bold',
+                color: '#fff'
               },
               predefinedPlacesDescription: {
                 color: '#1faadb'
+              },
+              poweredContainer: {
+                backgroundColor: 'rgba(0,0,0,0)'
               }
             }}
             ref={instance => {
               this.GooglePlacesRef = instance;
             }}
-            currentLocation // Will add a 'Current location' button at the top of the predefined places list
-            currentLocationLabel="Current location"
+            currentLocation={false} // Will add a 'Current location' button at the top of the predefined places list
+            currentLocationLabel="Localização atual"
             nearbyPlacesAPI="GooglePlacesSearch" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
             GoogleReverseGeocodingQuery={{
-              // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-            }}
+                // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+              }}
             GooglePlacesSearchQuery={{
               // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
               rankby: 'distance',
               types: 'food'
             }}
-            filterReverseGeocodingByTypes={[
-              'locality',
-              'administrative_area_level_3'
-            ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-            debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+            filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+            debounce={0} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
             renderLeftButton={() => (
               <Button transparent style={{ alignSelf: 'center' }}>
                 <Icon
